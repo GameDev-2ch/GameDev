4082739e2d61418c296b6ea84b09af7bdccc7b11
madmoonner <madmon@gmail.com>
2015-03-23 12:57:50 +0900

    fps表示処理の追加

--- UltimateGame/UltimateGame/main.cpp
+++ UltimateGame/UltimateGame/main.cpp
@@ -4,8 +4,12 @@
 int WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance,
 						LPSTR lpCmdLine, int nCmdShow )
 {
+
 	int color;
 
+	int lastFrameTime, timeCount, frameCount, currentTime;
+	float fps;
+	
 	// ウィンドウモードに変更
 	ChangeWindowMode( TRUE );
 
@@ -16,18 +20,43 @@ int WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance,
 		return -1 ;			// エラーが起きたら直ちに終了
 	}
 
-	while(1)
+	timeCount = 0;
+	frameCount = 0;
+	fps = 0.0f;
+	lastFrameTime = GetNowCount();
+
+	while (ProcessMessage() == 0 && CheckHitKey(KEY_INPUT_ESCAPE) == 0)
 	{
 		if( CheckHitKey( KEY_INPUT_ESCAPE ) == 1 )
 		{
 			break;
 		}
 
-		color = GetColor(GetRand(255), GetRand(255), GetRand(255));
+		SetDrawScreen(DX_SCREEN_BACK);
+		ClearDrawScreen();
 
-		DrawPixel( GetRand(512-1), GetRand(480-1), color );
+		currentTime = GetNowCount();
 
-		Sleep(1);
+
+		// ゲーム処理
+//		color = GetColor(GetRand(255), GetRand(255), GetRand(255));
+//		DrawPixel( GetRand(512-1), GetRand(480-1), color );
+
+
+
+		// fps表示
+		timeCount += currentTime - lastFrameTime;
+		frameCount++;
+		if (timeCount > 1000){
+			fps = frameCount * 1000.0f / timeCount;
+			frameCount = timeCount = 0;
+		}
+
+		DrawFormatString(0, 0, GetColor(255, 255, 255), _T("FPS:%.1f"), fps);
+		lastFrameTime = currentTime;
+
+		ScreenFlip();
+
 	}
 
 	DxLib_End() ;				// ＤＸライブラリ使用の終了処理
